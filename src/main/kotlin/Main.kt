7abf47e5a79044bef5c8845234ae010de34b298e
AndreyOvch <amo-rzn@ya.ru>
2020-10-13 00:35:51 +0300

    edit code

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,6 @@
 fun main() {
     val commissionCard = 0.75
-    val amount = 100_000*100
+    val amount = 100_000_00
     val minCommission = 35*100
     val commission = amount*commissionCard/100
     val resultCommission  =if (commission<=minCommission) minCommission else commission
